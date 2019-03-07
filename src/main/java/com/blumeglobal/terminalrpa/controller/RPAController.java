3685f10f6c5c418871409dac431ef6d1288f86e9
dion.lagos <dion.lagos@7490-DR3R5S2.dcli.local>
2019-03-07 01:26:20 -0800

    controller changes- need to return a value instead of void from service

--- src/main/java/com/blumeglobal/terminalrpa/controller/RPAController.java
+++ src/main/java/com/blumeglobal/terminalrpa/controller/RPAController.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blumeglobal.terminalrpa.service.IRPAService;
+import com.blumeglobal.terminalrpa.service.impl.RPAServiceImpl;
 import com.rez1.common.annotations.REZ1Logger;
 import com.rez1.common.annotations.REZ1PerformanceLogger;
 
@@ -15,10 +17,34 @@ import com.rez1.common.annotations.REZ1PerformanceLogger;
 @REZ1PerformanceLogger
 public class RPAController {
 	
+	private RPAServiceImpl myService;
+	
 	public static ResponseEntity<?> test(){
 		
 
 		return null;
 		
 	}
+	
+    @RequestMapping(value = "/evergreen")
+    public String evergreen() {
+    	try { 		
+    	   myService.terminalRPAEverportLA();
+    	   return "Completed!";
+
+    	} catch (Exception ex) {
+    		return ex.getMessage();
+    	}
+    }
+    
+    @RequestMapping(value = "/seattle")
+    public String seattle() {
+    	try { 		
+     	   myService.terminalRPASeattle46();
+     	   return "Completed!";
+
+     	} catch (Exception ex) {
+     		return ex.getMessage();
+     	}
+    }
 }
